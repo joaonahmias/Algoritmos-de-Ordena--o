f97e1bb9642106a0d6ebae0bee7949877aea605e
joaonahmias <joaovictornahmias@gmail.com>
2023-05-22 23:31:27 -0300

    First commit

---
+++ InsertionSort.js
@@ -0,0 +1,12 @@
+function InsertionSort(array){
+    for (let i = 1;i<array.length;i++){
+        let escolhido = array[i]
+        let j = i-1
+        while(j>=0&&array[j]>escolhido){
+            array[j+1]=array[j]
+            j=j-1
+        }
+        array[j+1]=escolhido
+    }
+    return array
+}
